d5e8e862d92296f8048468b9fc65cd2db16ac064
C-Skala <cjskala@gmail.com>
2022-06-12 20:27:14 -0400

    rough dinosaur class made

--- dinosaurs.py
+++ dinosaurs.py
@@ -1,6 +1,13 @@
+from robots import Robot
+
 class Dinosaur:
 
     def __init__(self) -> None:
         self.name = 'bones'
         self.attack_power = 20
         self.health = 100
+
+    def Dinosaur_attack(self):
+        print(f'{Dinosaur.name} attaks {Robot.name} with its powerfull jaws for {Dinosaur.attack_power}')
+        Robot.health -= Dinosaur.attack_power
+        print(f'{Robot.name} now has {Robot.health} remaining')
