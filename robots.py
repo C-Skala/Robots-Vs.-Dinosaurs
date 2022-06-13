2d4f0b98bc8ada1d8f84dbf20d5eb11ef691f024
C-Skala <cjskala@gmail.com>
2022-06-12 20:24:18 -0400

    rough robot class made

--- robots.py
+++ robots.py
@@ -8,3 +8,7 @@ class Robot:
         self.health = 100
         self.active_weapon = Weapon()
 
+    def robot_attack(self):
+        print(f'{Robot.name} attaks {Dinosaur.name} with {Weapon.name} for {Weapon.attack_power}')
+        Dinosaur.health -= Weapon.attack_power
+        print(f'{Dinosaur.name} now has {Dinosaur.health} remaining')
